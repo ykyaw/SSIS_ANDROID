3f63b22cb50fdd1c4471954a0fee4f0a8b370bdd
Sein Lai Hnin <e0533406@u.nus.edu>
2020-08-26 17:29:08 +0800

    UI for purchase request (supervisor)

--- app/src/main/java/iss/team1/ad/ssis_android/activity/PurchaseRequestDeatilActivity.java
+++ app/src/main/java/iss/team1/ad/ssis_android/activity/PurchaseRequestDeatilActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
