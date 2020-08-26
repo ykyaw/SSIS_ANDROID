e8ec6b9dc3533861bf0f76501cdbcce9c00ad3c2
Sein Lai Hnin <e0533406@u.nus.edu>
2020-08-26 17:43:46 +0800

    Hidng the nav bar for purchase req

--- app/src/main/java/iss/team1/ad/ssis_android/activity/PurchaseRequestDeatilActivity.java
+++ app/src/main/java/iss/team1/ad/ssis_android/activity/PurchaseRequestDeatilActivity.java
@@ -57,6 +57,8 @@ public class PurchaseRequestDeatilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_request_deatil);
 
